2d72c10644348a9a9d57c49d799b612b72384c4f
Jorge Amat Soler <amjsoler@gmail.com>
2024-02-20 10:10:05 +0100

    2021-07

--- 2021-03.test.js
+++ 2021-03.test.js
@@ -2,20 +2,58 @@ import {test, expect} from 'vitest'
 
 const isValid = (letter) => {
     let stack = []
+    const openers = ["{", "(", "["]
+    const closers = ["}", ")", "]"]
 
-    const opener = ['(', '[', '{']
-    const closer = [')', ']', '}']
+    let iteratorAux = 0
 
-    for (let i = 0; i < letter.length; i++) {
-        if (opener.includes(letter[i])) {
-            stack.push(letter[i])
-        } else if (closer.includes(letter[i])) {
-            if (stack.length === 0) return false
-            if (closer.indexOf(letter[i]) !== opener.indexOf(stack.pop())) return false
+    for(let i=0;i<letter.lenght;i++) {
+        //El primer elemento del stack tiene que ser un paréntesis de apertura, no puede ser otra cosa
+        if(openers.includes(letter[i])){
+            if(stack.length === 0){
+                if(letter[i] === "("){
+                    stack.push(letter[i])
+                }else{
+                    return false;
+                }
+            }else{
+                //Se puede añadir tantos elementos de apertura como se quiera
+                stack.push(letter[i])
+            }
         }
-    }
 
-    return true
+        //Cuando se ecuentre un elemento de cierre, el stack no puede estar vacío y
+        else if(closers.includes(letter[i])){
+            if(stack.length === 0){
+                return false
+            }else{
+                //el último elemento del stack, ha de ser el mismo elemento pero de apertura
+                const indexCloserFound = closers.indexOf(letter[i])
+                
+                if(openers[indexCloserFound] === stack.slice(-1)[0]){
+                    //Comprobamos si es un bloque vacío
+                    if(i-iteratorAux === 1){
+                        return false
+                    }else{
+                        //Si no, quitamos el ultimo elemento
+                        stack.pop()
+                    }
+
+                }else{
+                    return false
+                }
+            }
+        }
+
+        iteratorAux = i
+    }
+    
+    //Al finalizar el bucle, el stack debe estar vacío
+    if(stack.length === 0){
+        return true
+    }else{
+        return false
+    }
 }
 
 test('"bici coche (balón) bici coche peluche" // -> ✅', () => {
---
+++ 2021-07.test.js
@@ -0,0 +1,79 @@
+import {test, expect} from 'vitest'
+
+const contains = (store, product) => {
+    let encontrado = false
+
+    //Comprobamos si el store es un objeto
+    if(typeof(store) === "object"){
+        console.log("objecto")
+        //Si lo es, sacamos sus keys iteramos entre ellas
+        const keys = Object.keys(store)
+        keys.forEach(item => {
+            //Si la key es un objeto a su vez, llamamos a contains con el substore
+            if(typeof(store[item]) === "object"){
+                if(contains(store[item], product)){
+                    //Si obtenemos true, lo devolvemos
+                    encontrado = true
+                }
+            }
+            //Si es una cadena, comprobamos si es el producto
+            else if(store[item] == product){
+                console.log("Encontrado = true")
+                encontrado = true
+            }
+        })
+    }
+
+    return encontrado
+}
+
+test('almacén 1, contains camiseta -> true', () => {
+    const almacen = {
+        'estanteria1': {
+          'cajon1': {
+            'producto1': 'coca-cola',
+            'producto2': 'fanta',
+            'producto3': 'sprite'
+          }
+        },
+        'estanteria2': {
+          'cajon1': 'vacio',
+          'cajon2': {
+            'producto1': 'pantalones',
+            'producto2': 'camiseta' // <- ¡Está aquí!
+          }
+        }
+      }
+                  
+    expect(contains(almacen, 'camiseta')).toBe(true)
+})
+
+test('almacén 2, contains gameboy -> false', () => {
+    const otroAlmacen = 
+    {
+    'baul': {
+        'fondo': {
+            'objeto': 'cd-rom',
+            'otro-objeto': 'disquette',
+            'otra-cosa': 'mando'
+        }
+        }
+    }
+                  
+    expect(  contains(otroAlmacen, 'gameboy')).toBe(false)
+})
+
+test('almacén 3, contains gameboy -> false', () => {
+    const otroAlmacen = 
+    {
+    'baul': {
+        'fondo': {
+            'objeto': 'cd-rom',
+            'gameboy': 'disquette',
+            'otra-cosa': 'mando'
+        }
+        }
+    }
+                  
+    expect(  contains(otroAlmacen, 'gameboy')).toBe(false)
+})
