68f1284bfabebdfda381611a4438d7d643d9ab95
Jorge Amat Soler <amjsoler@gmail.com>
2024-02-20 14:56:32 +0100

    2021-05

--- 2021-05.test.js
+++ 2021-05.test.js
@@ -1,28 +1,53 @@
 import {test, expect} from 'vitest'
 
 const daysToXmas = (date) => {
-    if(typeof(date) === "undefined"){
-        throw new Error("daysToXmas expects date parameter")
+    const xMasDate = new Date('Dec 25, 2021')
+
+    const dateDiff = (xMasDate.getTime()-date.getTime())/1000/60/60/24
+
+    if(dateDiff >= 0 && dateDiff <= 1){
+        if(date.getDay() === xMasDate.getDay()){
+            return 0
+        }else{
+            return 1
+        }
+    }else{
+        return Math.ceil(dateDiff)
     }
-    if(!(date instanceof Date)){
-        throw new Error("date parameter must be a date")
-    }
-    
-    return 1
 }
 
-test('must be a function', () => {
-    expect(typeof(daysToXmas)).toBe("function")
+test('daysToXmas(new Date("Dec 1, 2021") // 24', () => {
+    expect(daysToXmas(new Date('Dec 1, 2021'))).toBe(24)
 })
 
-test('function accepts one parameter', () => {
-    expect(() => daysToXmas()).toThrowError("daysToXmas expects date parameter")
+test("daysToXmas(new Date('Dec 24, 2021 00:00:01')) //1", () => {
+    expect(daysToXmas(new Date('Dec 24, 2021 00:00:01'))).toBe(1)
 })
 
-test('the parameter must be a date', () => {
-    expect(() => daysToXmas("no date parameter")).toThrowError("date parameter must be a date")
+test("daysToXmas(new Date('Dec 24, 2021 23:59:59')) //1", () => {
+    expect(daysToXmas(new Date('Dec 24, 2021 23:59:59'))).toBe(1)
 })
 
-test('function must return an int', () => {
-    expect(typeof(daysToXmas(new Date()))).toBe("number")
+test('daysToXmas(new Date("December 20, 2021 03:24:00")) //5', () => {
+    expect(daysToXmas(new Date("December 20, 2021 03:24:00"))).toBe(5)
+})
+
+test("daysToXmas(new Date(new Date('Dec 25, 2021')) //0", () => {
+    expect(daysToXmas(new Date('Dec 25, 2021'))).toBe(0)
+})
+
+test("daysToXmas(new Date('Dec 26, 2021')) //-1", () => {
+    expect(daysToXmas(new Date('Dec 26, 2021'))).toBe(-1)
+})
+
+test("daysToXmas(new Date('Dec 31, 2021')) //-6", () => {
+    expect(daysToXmas(new Date('Dec 31, 2021'))).toBe(-6)
+})
+
+test("daysToXmas(new Date('Jan 1, 2022 00:00:01')) //-7", () => {
+    expect(daysToXmas(new Date('Jan 1, 2022 00:00:01'))).toBe(-7)
+})
+
+test("daysToXmas(new Date('Jan 1, 2022 23:59:59')) //-7", () => {
+    expect(daysToXmas(new Date('Jan 1, 2022 23:59:59'))).toBe(-7)
 })
